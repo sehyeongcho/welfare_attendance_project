5bcfc5c91bc91b4b5b5ccdce48a10ed9dc45834a
Sehyeong Cho <21400714@handong.ac.kr>
2023-06-05 02:04:03 +0900

    feat: push untracked files

---
+++ lib/institution/detailpage.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import 'editteacherpage.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({Key? key /*, required this.data, required this.docId*/})
+      : super(key: key);
+
+  // final Map<String, dynamic> data;
+  // final String docId;
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   context.read<MyAppState>().initFavorite();
+  // }
+
+  String convertTime(dynamic time) {
+    final timestamp = time;
+    final date = DateTime.fromMillisecondsSinceEpoch(
+      timestamp.seconds * 1000 + (timestamp.nanoseconds / 1000000).round(),
+    );
+
+    return date.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // create a Floating Action Button
+      // before adding to wishlist: shopping_cart icon
+      // after adding to wishlist: check icon
+      // floatingActionButton: FloatingActionButton(
+      //   child: Consumer<MyAppState>(
+      //     builder: (context, appState, child) {
+      //       return (appState.wishlistState['wishlist'] == null ||
+      //               !appState.wishlistState['wishlist'].contains(widget.docId))
+      //           ? const Icon(Icons.shopping_cart)
+      //           : const Icon(Icons.check);
+      //     },
+      //   ),
+
+      //   // when the floating action button is pressed, the item is added to wishlist
+      //   onPressed: () {
+      //     context.read<MyAppState>().toggleFavorite(widget.docId);
+      //   },
+      // ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('강사정보'),
+        actions: <Widget>[
+          // When you click the pencil icon(Icons.create), you can modify(update) the information of the item
+          IconButton(
+            icon: const Icon(
+              Icons.create,
+              semanticLabel: 'edit',
+            ),
+            onPressed: () {
+              // if (FirebaseAuth.instance.currentUser!.uid ==
+              //     widget.data['uid']) {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(
+              //     builder: (context) =>
+              //         EditPage(data: widget.data, docId: widget.docId),
+              //   ),
+              //   );
+              // }
+
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => EditTeacherPage()),
+              );
+            },
+          ),
+
+          // When you click the trash icon(Icons.delete), you can delete the item
+          IconButton(
+            icon: const Icon(
+              Icons.delete,
+              semanticLabel: 'delete',
+            ),
+            onPressed: () async {
+              // If the UID is different from yours (if you are not the author of the post), it should not be Modified or Deleted (you can use Firestore Document Fields or Security Rules)
+              // if (FirebaseAuth.instance.currentUser!.uid ==
+              //     widget.data['uid']) {
+              //   showDialog<String>(
+              //     context: context,
+              //     builder: (BuildContext context) => AlertDialog(
+              //       title: const Text('Delete'),
+              //       content: const Text('Are you sure you want to delete it?'),
+              //       actions: <Widget>[
+              //         TextButton(
+              //           onPressed: () => Navigator.pop(context, 'Cancel'),
+              //           child: const Text('Cancel'),
+              //         ),
+              //         TextButton(
+              //           onPressed: () async {
+              //             db
+              //                 .collection("product")
+              //                 .doc(widget.docId)
+              //                 .delete()
+              //                 .then(
+              //                   (doc) => print("Document deleted"),
+              //                   onError: (e) =>
+              //                       print("Error updating document $e"),
+              //                 );
+
+              //             db.collection("user").get().then((querySnapshot) {
+              //               for (var docSnapshot in querySnapshot.docs) {
+              //                 docSnapshot.reference.update({
+              //                   "wishlist":
+              //                       FieldValue.arrayRemove([widget.docId]),
+              //                 });
+              //               }
+              //             });
+
+              //             await storageRef.child(widget.docId).delete();
+
+              //             Navigator.pop(context, 'OK');
+              //             Navigator.pop(context);
+              //           },
+              //           child: const Text('OK'),
+              //         ),
+              //       ],
+              //     ),
+              //   );
+              // }
+            },
+          ),
+        ],
+      ),
+
+      // Detail page should have Product Image, Name, Price and Description
+      body: ListView(
+        children: [
+          // Image
+          Lottie.network(
+            'https://assets2.lottiefiles.com/packages/lf20_h9rxcjpi.json',
+            width: 300.0,
+            height: 300.0,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      // Name
+                      child: Text(
+                        '강사 이름',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    // StreamBuilder(
+                    //   stream: db
+                    //       .collection('product')
+                    //       .doc(widget.docId)
+                    //       .snapshots(),
+                    //   builder: (context, snapshot) {
+                    //     return FavoriteWidget(
+                    //         data: widget.data, docId: widget.docId);
+                    //   },
+                    // ),
+                  ],
+                ),
+                SizedBox(height: 12.0),
+
+                // Price
+                Text('강사 전화번호'),
+                SizedBox(height: 12.0),
+                Text('강사 생년월일'),
+                SizedBox(height: 12.0),
+                Divider(
+                  thickness: 2.0,
+                ),
+                SizedBox(height: 12.0),
+
+                // Description
+                Text('강사 강의목록'),
+                SizedBox(height: 12.0),
+
+                // Show UID & creation time & recent update time (Use FieldValue.serverTimestamp())
+                // Text("Creator: ${widget.data['uid']}"),
+                // Text("${convertTime(widget.data['creationtime'])} Created"),
+                // Text(
+                //     "${convertTime(widget.data['recentupdatetime'])} Modified"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/institution/editteacherpage.dart
@@ -0,0 +1,299 @@
+import 'package:flutter/material.dart';
+import 'package:welfare_attendance_project/app_state.dart';
+import 'package:welfare_attendance_project/institution/qrscanner.dart';
+import 'calender.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class EditTeacherPage extends StatefulWidget {
+  EditTeacherPage({Key? key}) : super(key: key);
+
+  @override
+  State<EditTeacherPage> createState() => _EditTeacherPageState();
+}
+
+class _EditTeacherPageState extends State<EditTeacherPage> {
+  Map<String, dynamic> _maplist = Map();
+
+  final _formKey = GlobalKey<FormState>();
+
+  final _nameController = TextEditingController();
+
+  var _calenderController = TextEditingController();
+
+  final _phonenumberController = TextEditingController();
+
+  final _classController = TextEditingController();
+  String dropdownValue = '';
+  String qrcode = '';
+
+  void get_qrcode(String? code) {
+    setState(() {
+      this.qrcode = code!;
+    });
+  }
+
+  void select_canlender(String calendar) {
+    setState(() {
+      this._calenderController = TextEditingController(text: calendar);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<ApplicationState>();
+    _maplist = appState.maplist!;
+    if (dropdownValue == '')
+      dropdownValue = _maplist.keys.length == 0 ? '' : _maplist.keys.first;
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('강사수정'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 12.0),
+              Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    // crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // const SizedBox(height: 12.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          SizedBox(
+                            width: 100.0,
+                            // height: 50.0,
+                            child: ElevatedButton(
+                                onPressed: () async {
+                                  if (qrcode == '') {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                        const SnackBar(
+                                            content: Text(
+                                                '등록할 강사의 QR 코드를 인증해 주세요')));
+                                    return;
+                                  }
+
+                                  if (_formKey.currentState!.validate()) {
+                                    late var check;
+                                    await FirebaseFirestore.instance
+                                        .collection('teachers')
+                                        .where('teacheruid', isEqualTo: qrcode)
+                                        .get()
+                                        .then((value) {
+                                      check = value.docs.length;
+                                    });
+                                    // print('null : ${nullcheck}');
+                                    if (check > 0) {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(const SnackBar(
+                                              content: Text('이미 등록된 강사입니다')));
+                                      return;
+                                    }
+                                    late final sheetid;
+                                    _maplist.forEach((key, value) {
+                                      if (key == dropdownValue)
+                                        sheetid = value[1];
+                                    });
+                                    await FirebaseFirestore.instance
+                                        .collection('manager')
+                                        .doc(FirebaseAuth
+                                            .instance.currentUser!.uid)
+                                        .update(<String, dynamic>{
+                                      dropdownValue: [true, sheetid],
+                                    });
+
+                                    await FirebaseFirestore.instance
+                                        .collection('manager')
+                                        .doc(FirebaseAuth
+                                            .instance.currentUser!.uid)
+                                        .collection('teacher')
+                                        .add(<String, dynamic>{
+                                      'teacheruid': qrcode,
+                                      'name': _nameController.text.trim(),
+                                      'birthday':
+                                          _calenderController.text.trim(),
+                                      'phonenumber':
+                                          _phonenumberController.text.trim(),
+                                    });
+
+                                    await FirebaseFirestore.instance
+                                        .collection('teachers')
+                                        .doc(qrcode)
+                                      ..set(<String, dynamic>{
+                                        'teacheruid': qrcode,
+                                        dropdownValue: sheetid
+                                      });
+                                    Navigator.of(context).pop();
+                                  }
+                                },
+                                child: const Text('수정')),
+                          ),
+                          // SizedBox(
+                          //   width: 20,
+                          // ),
+                          SizedBox(
+                            width: 100.0,
+                            // height: 50.0,
+                            child: FilledButton(
+                              child: const Text('초기화'),
+                              onPressed: () {
+                                _nameController.clear();
+                                _phonenumberController.clear();
+                                _classController.clear();
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 12.0),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return '이름을 입력해주세요';
+                          }
+                          return null;
+                        },
+                        controller: _nameController,
+                        decoration: const InputDecoration(
+                          // filled: true,
+                          labelText: '이름',
+                        ),
+                      ),
+                      const SizedBox(height: 12.0),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return '날짜를 선택해주세요';
+                                }
+                                return null;
+                              },
+                              controller: _calenderController,
+                              decoration: const InputDecoration(
+                                // filled: true,
+                                labelText: '날짜',
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 12.0),
+                          Calender(
+                            selectedcalender: select_canlender,
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 12.0),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return '전화번호를 입력해주세요';
+                          }
+                          RegExp numberRegex = RegExp(r'^[0-9]+$');
+                          if (!numberRegex.hasMatch(value!)) {
+                            return '숫자만 입력해주세요';
+                          }
+                          return null;
+                        },
+                        controller: _phonenumberController,
+                        decoration: const InputDecoration(
+                          // filled: true,
+                          labelText: '전화번호',
+                        ),
+                      ),
+                      const SizedBox(height: 12.0),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return '강의를 선택해주세요';
+                          }
+                          return null;
+                        },
+                        controller: _classController,
+                        decoration: const InputDecoration(
+                          // filled: true,
+                          labelText: '강의',
+                        ),
+                      ),
+                      const SizedBox(height: 12.0),
+                      _maplist.keys.length > 0
+                          ? DropdownButton<String>(
+                              value: dropdownValue,
+                              icon: const Icon(Icons.arrow_downward),
+                              elevation: 16,
+                              style: const TextStyle(color: Colors.deepPurple),
+                              underline: Container(
+                                height: 2,
+                                color: Colors.deepPurpleAccent,
+                              ),
+                              onChanged: (String? value) {
+                                // This is called when the user selects an item.
+                                setState(() {
+                                  dropdownValue = value!;
+                                });
+                              },
+                              items: _maplist.keys
+                                  .map<DropdownMenuItem<String>>(
+                                      (String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                            )
+                          : const Text('모든 강의가 할당됐거나 등록된 강의가 없습니다'),
+                      const SizedBox(height: 12.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          qrcode == ''
+                              ? const Expanded(
+                                  child: Text('강사의 QR 코드를 인증해 주세요'))
+                              : const Expanded(child: Text('인증되었습니다')),
+                          const SizedBox(width: 12.0),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => QRScanner(
+                                          getQrcode: get_qrcode,
+                                        )),
+                              );
+                            },
+                            child: const Center(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.qr_code_2),
+                                  SizedBox(width: 4.0),
+                                  Text('인증'),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
