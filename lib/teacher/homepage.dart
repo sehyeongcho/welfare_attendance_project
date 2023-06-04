3b8c4531925e8fae953bcd34ffebe7f53d2832c0
Sehyeong Cho <21400714@handong.ac.kr>
2023-06-05 02:40:28 +0900

    design: update teacher home page

--- lib/teacher/homepage.dart
+++ lib/teacher/homepage.dart
@@ -19,8 +19,26 @@ class _HomePageState extends State<HomePage> {
   late final sheet;
   late List<bool> _selectedIcon;
   final List<Widget> icons = <Widget>[
-    const Icon(Icons.circle),
-    const Icon(Icons.clear ),
+    const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.circle_outlined,
+          size: 16.0,
+        ),
+        Text('출석'),
+      ],
+    ),
+    const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.clear,
+          size: 16.0,
+        ),
+        Text('결석'),
+      ],
+    ),
   ];
   int _grid_or_list = 1; //false = grid
 
@@ -39,46 +57,51 @@ class _HomePageState extends State<HomePage> {
     var appState = context.watch<ApplicationState>();
     final ThemeData theme = Theme.of(context);
     // appState.getdata(sheet.data);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('main'),
-        leading: IconButton(
-          icon: const Icon(
-            Icons.person,
-            semanticLabel: 'profile',
+    return WillPopScope(
+      onWillPop: () {
+        return Future(() => false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('강의목록'),
+          leading: IconButton(
+            icon: const Icon(
+              Icons.person,
+              semanticLabel: 'profile',
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ProfilePage()),
+              );
+            },
           ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ProfilePage()),
-            );
-          },
         ),
-      ),
-      body: Column(
-        children: [
-          Text(FirebaseAuth.instance.currentUser!.uid),
-          ElevatedButton(
-              onPressed: () async {
-                // sheet.find_sheetid();
-                // sheet.downloadcsv();
+        body: Column(
+          children: [
+            Text(FirebaseAuth.instance.currentUser!.uid),
+            ElevatedButton(
+                onPressed: () async {
+                  // sheet.find_sheetid();
+                  // sheet.downloadcsv();
 
-                // await sheet.initalWorkSheet();
-                // sheet.insertRow();
+                  // await sheet.initalWorkSheet();
+                  // sheet.insertRow();
 
-                appState.getdata(sheet.data);
-                _selectedIcon = <bool>[false, true];
-              },
-              child: Text('sheet')),
-          Expanded(
-            child: appState.exceldata == null
-                ? CircularProgressIndicator()
-                : ListView(
-                    padding: const EdgeInsets.all(16.0),
-                    children: cardtolist(theme)
-                    ),
-          )
-        ],
+                  appState.getdata(sheet.data);
+                  _selectedIcon = <bool>[false, true];
+                },
+                child: Text('sheet')),
+            Expanded(
+              child: appState.exceldata == null
+                  ? CircularProgressIndicator()
+                  : ListView(
+                      padding: const EdgeInsets.all(16.0),
+                      children: cardtolist(theme)),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -95,37 +118,45 @@ class _HomePageState extends State<HomePage> {
           _selectedIcon = [false, true];
         list.add(Card(
           child: Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(16.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Column(
-                    children: [Text('${data[0]}'), Text('${data[1]}')],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '${data[0]}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 4.0),
+                      Text('${data[1]}'),
+                    ],
                   ),
-                  SizedBox(width: 20,),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 16.0, 16.0, 0),
-                    child: ToggleButtons(
-                      direction: Axis.horizontal,
-                      onPressed: (int index) {
-                        setState(() {
-                          // The button that is tapped is set to true, and the others to false.
-                          for (int i = 0; i < _selectedIcon.length; i++) {
-                            // if(data[2] == 'o')
-                            //  index = index!;
-                            _selectedIcon[i] = i == index;
-                          }
-                          _grid_or_list = index;
-                        });
-                      },
-                      borderRadius: const BorderRadius.all(Radius.circular(8)),
-                      selectedBorderColor: theme.colorScheme.primary,
-                      selectedColor: theme.colorScheme.primary,
-                      fillColor: Colors.blue[200],
-                      color: Colors.grey,
-                      isSelected: _selectedIcon,
-                      children: icons,
-                    ),
+                  // SizedBox(width: 20,),
+                  ToggleButtons(
+                    direction: Axis.horizontal,
+                    onPressed: (int index) {
+                      setState(() {
+                        // The button that is tapped is set to true, and the others to false.
+                        for (int i = 0; i < _selectedIcon.length; i++) {
+                          // if(data[2] == 'o')
+                          //  index = index!;
+                          _selectedIcon[i] = i == index;
+                        }
+                        _grid_or_list = index;
+                      });
+                    },
+                    borderRadius: const BorderRadius.all(Radius.circular(8)),
+                    selectedBorderColor: theme.colorScheme.primary,
+                    selectedColor: theme.colorScheme.primary,
+                    fillColor: Colors.blue[200],
+                    color: Colors.grey,
+                    isSelected: _selectedIcon,
+                    children: icons,
                   ),
                   // Text('${data[2]}')
                 ],
